7b061165eb4c57c30154d42a1ef84e7c26898afa
thefint <fintancross@gmail.com>
2026-06-22 21:59:27 +0100

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -20,9 +20,6 @@ export default async function handler(req, res) {
       }),
     });
 
-    const data = await response.json();
+const data = await response.json();
+    console.log("Anthropic response:", JSON.stringify(data));
     res.status(200).json(data);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-}
